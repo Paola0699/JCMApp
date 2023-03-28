a971104e5c1a62ce465c70894c678af3260e0308
Paola Perez Valencia <paolaper@cisco.com>
2023-03-28 13:51:09 -0600

    update deleted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import AppRouter from './routers/AppRouter';
 import { Provider } from 'react-redux';
 import { persistor, store } from './store/store';
-import { ThemeProvider } from '@emotion/react';
+import { ThemeProvider } from '@mui/material';
 import { JCMTheme } from './theme';
 import './App.css';
 import { PersistGate } from 'redux-persist/integration/react';
