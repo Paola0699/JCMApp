767876ea3d624ec74cd2c323ee2f13284f37e8ab
Paola Perez Valencia <paolaper@cisco.com>
2023-03-28 13:14:30 -0600

    UPDATE More lintern errors fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import AppRouter from './routers/AppRouter';
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux';
 import { persistor, store } from './store/store';
 import { ThemeProvider } from '@emotion/react';
 import { JCMTheme } from './theme';
-import './App.css'
+import './App.css';
 import { PersistGate } from 'redux-persist/integration/react';
 
 function App() {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor} >
+      <PersistGate loading={null} persistor={persistor}>
         <ThemeProvider theme={JCMTheme}>
           <AppRouter />
         </ThemeProvider>
