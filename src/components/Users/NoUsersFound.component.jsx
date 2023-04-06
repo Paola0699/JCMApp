5cc8afb277147bd9c3a6d60574afe0b604de406a
Paola Perez Valencia <paola.pv1999@gmail.com>
2023-04-06 14:38:19 -0600

    UPDATE Minnor change

--- src/components/Users/NoUsersFound.component.jsx
+++ src/components/Users/NoUsersFound.component.jsx
@@ -3,7 +3,7 @@ import NoUsersImg from '../../assets/images/no-users2.jpg';
 const NoUsersFound = () => {
   return (
     <Grid container direction={'column'} padding={5}>
-      <Grid item sx={12} display={'flex'} justifyContent={'center'}>
+      <Grid item xs={12} display={'flex'} justifyContent={'center'}>
         <img style={{ width: '25rem' }} src={NoUsersImg} alt="No users found" />
       </Grid>
       <Grid item display={'flex'} justifyContent={'center'}>
